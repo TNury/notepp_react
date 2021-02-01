393af087ca5d89af90bf19088ce80ce528c4ec34
TNury <yurdesou@gmail.com>
2021-02-01 15:56:33 -0300

    removed icon white space

--- src/js/components/app-components/navbar/navbar-item/navbar-item.jsx
+++ src/js/components/app-components/navbar/navbar-item/navbar-item.jsx
@@ -8,7 +8,7 @@ function NavbarItem ({text, icon}) {
   return (
     <div className="navbar-item">
       <button className="navbar-button">
-        <svg className="navbar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 80" x="0px" y="0px">
+        <svg xmlns="http://www.w3.org/2000/svg" className="navbar-icon" x="0px" y="0px" viewBox="2 2 59.82 60">
           <Icon icon={icon} />
         </svg>
         <span className="navbar-text">{text}</span>
