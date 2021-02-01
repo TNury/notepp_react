52a590dfbd2503c57d333136f31f93f10acb08b8
TNury <yurdesou@gmail.com>
2021-02-01 13:58:11 -0300

    navbar component

--- src/js/components/app-components/navbar/navbar.jsx
+++ src/js/components/app-components/navbar/navbar.jsx
@@ -1,15 +1,21 @@
+// LIBRARIES
 import React from 'react-dom';
+// COMPONENTS
+import {NavbarItem} from './navbar-item/navbar-item.jsx';
 
-// STYLES 
-
-import '../../../../styles/css/navbar.css';
-
-function navbar() {
+function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-items">
-
+        <NavbarItem text="Notepp" icon="logo"/>
+        <NavbarItem text="Notes" icon="notes"/>
+        <NavbarItem text="Search" icon="search"/>
+        <NavbarItem text="Themes" icon="themes"/>
+        <NavbarItem text="Logout" icon="logout"/>
+        <NavbarItem text="Configs" icon="configs"/>
       </div>
     </nav>
   )
 }
+
+export {Navbar};
