c7b4d8dfbdd2013c26e4521261b41e16c20000db
TNury <yurdesou@gmail.com>
2021-02-01 09:36:20 -0300

    new navbar component

---
+++ src/js/components/app-components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react-dom';
+
+// STYLES 
+
+import '../../../../styles/css/navbar.css';
+
+function navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-items">
+
+      </div>
+    </nav>
+  )
+}
