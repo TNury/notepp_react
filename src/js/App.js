4c594b1acf48e894cdc367107323d8dc809b49cf
TNury <yurdesou@gmail.com>
2021-02-01 09:36:13 -0300

    imported styles and navbar component

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,14 @@
+// STYLES
+import '../styles/css/App.css';
+import '../styles/css/normalize.css';
+// COMPONENTS
+import './components/app-components/navbar/navbar.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      
     </div>
   );
 }
