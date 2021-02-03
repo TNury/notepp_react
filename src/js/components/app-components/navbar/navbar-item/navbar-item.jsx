5daca351d05967bbd4c01590ec9664368475db6e
TNury <yurdesou@gmail.com>
2021-02-03 08:42:37 -0300

    changed icons from Nounproject to Fontawesome

--- src/js/components/app-components/navbar/navbar-item/navbar-item.jsx
+++ src/js/components/app-components/navbar/navbar-item/navbar-item.jsx
@@ -1,16 +1,14 @@
 // LIBRARIES
 import React from 'react-dom';
 // COMPONENTS
-import { Icon } from './navbar-item__icons/navbar-item__icons.jsx';
+// import { Icon } from './navbar-item__icons/navbar-item__icons.jsx';
 
 
 function NavbarItem ({text, icon}) {
   return (
     <div className="navbar-item">
       <button className="navbar-button">
-        <svg xmlns="http://www.w3.org/2000/svg" className="navbar-icon" x="0px" y="0px" viewBox="2 2 59.82 60">
-          <Icon icon={icon} />
-        </svg>
+        <i className={`navbar-icon fas ${icon}`}></i>
         <span className="navbar-text">{text}</span>
       </button>
     </div>
