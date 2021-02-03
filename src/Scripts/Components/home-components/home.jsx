20f2efbfcabcee9c5579a103e80d9bfa2563ca5e
TNury <yurdesou@gmail.com>
2021-02-03 14:45:05 -0300

    new homepage component path change

--- src/Scripts/Components/home-components/home.jsx
+++ src/Scripts/Components/home-components/home.jsx
@@ -1,6 +1,17 @@
+import React from 'react';
+
+// COMPONENTS
+import { Header } from './header/header.jsx';
+
+// STYLES
+import '../../../Styles/css/home.css';
 
 function Home() {
   return (
-    <></>
+    <div className="Home">
+      <Header />
+    </div>
   )
 }
+
+export {Home};
