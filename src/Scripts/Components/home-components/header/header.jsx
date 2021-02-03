aa5bd4e1d79c425d4594bbbd7bea0d6535030547
TNury <yurdesou@gmail.com>
2021-02-03 15:35:53 -0300

    changed class names to prevent style colision

--- src/Scripts/Components/home-components/header/header.jsx
+++ src/Scripts/Components/home-components/header/header.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Header() {
   return (
     <header className="header">
-      <nav className="navbar">
-        <a className="navbar__link navbar__logo" href="/">Notepp</a>
-        <a className="navbar__link" href="/">About</a>
-        <a className="navbar__link navbar__login" href="/">Sign in with Google</a>
+      <nav className="home-navbar">
+        <a className="home-navbar__link home-navbar__logo" href="/">Notepp</a>
+        <a className="home-navbar__link" href="/">About</a>
+        <a className="home-navbar__link home-navbar__login" href="/">Sign in with Google</a>
       </nav>
     </header>
   )
