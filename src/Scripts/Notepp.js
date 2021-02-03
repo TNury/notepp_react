1ff7cac585e496fab14c018688a8a0ddc864c7f4
TNury <yurdesou@gmail.com>
2021-02-03 14:55:16 -0300

    testing still

--- src/Scripts/Notepp.js
+++ src/Scripts/Notepp.js
@@ -1,13 +1,14 @@
-// PROTO
+// ROUTER
 import {Route} from 'react-router-dom';
 // COMPONENTS
-import {App} from './Components/app-components/app.jsx';
+// import {App} from './Components/app-components/app.jsx';
+import {Home} from './Components/home-components/home.jsx';
 
 function Notepp() {
   return (
-    <>
-      <App />
-    </>
+    <div>
+      <Home />
+    </div>
   );
 }
 
