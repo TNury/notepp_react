73343e072987c58a6df4f07596c0531cbf2970aa
TNury <yurdesou@gmail.com>
2021-02-03 14:44:55 -0300

    changed href value and fixed className

--- src/Scripts/Components/home-components/header/header.jsx
+++ src/Scripts/Components/home-components/header/header.jsx
@@ -1,11 +1,12 @@
+import React from 'react';
 
 function Header() {
   return (
-    <header class="header">
-      <nav class="navbar">
-        <a class="navbar__link navbar__logo" href="#">Notepp</a>
-        <a class="navbar__link" href="#">About</a>
-        <a class="navbar__link navbar__login" href="#">Sign in with Google</a>
+    <header className="header">
+      <nav className="navbar">
+        <a className="navbar__link navbar__logo" href="/">Notepp</a>
+        <a className="navbar__link" href="/">About</a>
+        <a className="navbar__link navbar__login" href="/">Sign in with Google</a>
       </nav>
     </header>
   )
