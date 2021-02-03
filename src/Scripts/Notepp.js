1f2f54c4ae5bf5f33529544298e7737551d5b1f3
TNury <yurdesou@gmail.com>
2021-02-03 15:39:36 -0300

    added style imports

--- src/Scripts/Notepp.js
+++ src/Scripts/Notepp.js
@@ -1,13 +1,18 @@
 // ROUTER
 import {Route} from 'react-router-dom';
 // COMPONENTS
-// import {App} from './Components/app-components/app.jsx';
+import {App} from './Components/app-components/app.jsx';
 import {Home} from './Components/home-components/home.jsx';
+
+// NORMALIZER STYLES 
+import '../Styles/css/normalizer.css';
+import '../Styles/css/Notepp.css';
+
 
 function Notepp() {
   return (
     <div>
-      <Home />
+      <App />
     </div>
   );
 }
