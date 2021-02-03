9b92fb197fd895912ee312a0d1f2e3bec7ce4bc4
TNury <yurdesou@gmail.com>
2021-02-03 14:11:50 -0300

    Another change to app framework

---
+++ src/Scripts/Notepp.js
@@ -0,0 +1,14 @@
+// PROTO
+import {Route} from 'react-router-dom';
+// COMPONENTS
+import {App} from './Components/app-components/app.jsx';
+
+function Notepp() {
+  return (
+    <>
+      <App />
+    </>
+  );
+}
+
+export {Notepp};
