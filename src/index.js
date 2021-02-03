b447f25c0869939f806f12391ecfd7dab21dd879
TNury <yurdesou@gmail.com>
2021-02-03 14:55:05 -0300

    fixed poor destructuring and removed unsed styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 // LIBRARIES
-import {React} from 'react';
-import {ReactDOM} from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-// STYLES
-import './styles/css/index.css';
 // COMPONENTS
 
 import {Notepp} from './Scripts/Notepp.js';
