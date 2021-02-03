e99ef81ad92b19ff8e96984c65fe97a205ffb882
TNury <yurdesou@gmail.com>
2021-02-03 14:10:47 -0300

    new home page and header component

---
+++ src/Scripts/Components/home-components/header/header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+  return (
+    <header class="header">
+      <nav class="navbar">
+        <a class="navbar__link navbar__logo" href="#">Notepp</a>
+        <a class="navbar__link" href="#">About</a>
+        <a class="navbar__link navbar__login" href="#">Sign in with Google</a>
+      </nav>
+    </header>
+  )
+}
+
+export {Header};
