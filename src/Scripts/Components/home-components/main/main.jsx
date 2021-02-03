e5b390a802984bc391c7841e8643ec5eef34d90d
TNury <yurdesou@gmail.com>
2021-02-03 14:54:24 -0300

    new main component for homepage

---
+++ src/Scripts/Components/home-components/main/main.jsx
@@ -0,0 +1,24 @@
+
+function Main() {
+  return(
+    <main className="content-wrapper">
+      <section className="content-header">
+        <h1>Simply Simple</h1>
+        <p><span>NOTEPP</span> is a simple note-taking web app i wrote for my portfolio.</p>
+      </section>
+      <section className="content-p">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur unde incidunt tempore.
+          Dolorum omnis, reiciendis, dolore eum eius vero accusantium qui repellendus commodi rerum ad
+          iste aliquid. Id eaque autem perspiciatis dignissimos tempora, accusantium explicabo nobis porro
+          modi suscipit labore dicta quisquam minus cum possimus quidem saepe sed velit ad.
+        </p>
+      </section>
+      <section className="content-image">
+        <div></div>
+      </section>
+    </main>
+  )
+}
+
+export {Main};
