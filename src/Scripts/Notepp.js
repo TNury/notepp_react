1ce6e963f78838dfb42095df460a38d8dbc86e0c
TNury <yurdesou@gmail.com>
2021-02-23 15:45:44 -0300

    changed path from about to signIn

--- src/Scripts/Notepp.js
+++ src/Scripts/Notepp.js
@@ -7,7 +7,7 @@ import {Route, Switch} from 'react-router-dom';
 // COMPONENTS
 import {App} from './Components/app-components/app.jsx';
 import {Home} from './Components/home-components/home.jsx';
-import {About} from './Components/about-components/about.jsx';
+import {SignIn} from './Components/signIn-component/signIn.jsx';
 
 // APP STYLES 
 import '../Styles/css/normalizer.css';
@@ -31,7 +31,7 @@ class Notepp extends React.Component {
         <Switch>
           <Route exact={true} path='/' component={Home}/>
           <Route exact={true} path='/app' component={App}/>
-          <Route exact={true} path='/about' component={About}/>
+          <Route exact={true} path='/signin' component={SignIn}/>
         </Switch>
       </>
     );
