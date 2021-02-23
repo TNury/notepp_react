c46ada9ef71cc7750646e1ab66cd42829ebaae8c
TNury <yurdesou@gmail.com>
2021-02-23 06:43:13 -0300

    formatting changes

--- src/Scripts/Notepp.js
+++ src/Scripts/Notepp.js
@@ -1,19 +1,21 @@
 // ROUTER
 import {Route} from 'react-router-dom';
+
 // COMPONENTS
 import {App} from './Components/app-components/app.jsx';
 import {Home} from './Components/home-components/home.jsx';
+import {About} from './Components/about-components/about.jsx';
 
-// NORMALIZER STYLES 
+// STYLES 
 import '../Styles/css/normalizer.css';
 import '../Styles/css/Notepp.css';
-
 
 function Notepp() {
   return (
     <>
       <Route exact={true} path='/' component={Home}/>
       <Route exact={true} path='/app' component={App}/>
+      <Route exact={true} path='/about' component={About}/>
     </>
   );
 }
