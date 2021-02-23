b692eac21e9e7c20e103735db86050fb988d9742
TNury <yurdesou@gmail.com>
2021-02-23 11:51:43 -0300

    extended Notepp to class component

--- src/Scripts/Notepp.js
+++ src/Scripts/Notepp.js
@@ -1,23 +1,41 @@
-// ROUTER
-import {Route} from 'react-router-dom';
+// REACT
+import React from 'react';
+
+// REACT-ROUTER COMPONENTS
+import {Route, Switch} from 'react-router-dom';
 
 // COMPONENTS
 import {App} from './Components/app-components/app.jsx';
 import {Home} from './Components/home-components/home.jsx';
 import {About} from './Components/about-components/about.jsx';
 
-// STYLES 
+// APP STYLES 
 import '../Styles/css/normalizer.css';
 import '../Styles/css/Notepp.css';
 
-function Notepp() {
-  return (
-    <>
-      <Route exact={true} path='/' component={Home}/>
-      <Route exact={true} path='/app' component={App}/>
-      <Route exact={true} path='/about' component={About}/>
-    </>
-  );
+class Notepp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <>
+        <Switch>
+          <Route exact={true} path='/' component={Home}/>
+          <Route exact={true} path='/app' component={App}/>
+          <Route exact={true} path='/about' component={About}/>
+        </Switch>
+      </>
+    );
+  }
 }
 
 export {Notepp};
