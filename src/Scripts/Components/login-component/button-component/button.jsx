b4509d71ea1abda66de1ce6aece787c5d2402b31
TNury <yurdesou@gmail.com>
2021-02-25 16:23:44 -0300

    added functionality to login with google button

--- src/Scripts/Components/login-component/button-component/button.jsx
+++ src/Scripts/Components/login-component/button-component/button.jsx
@@ -1,10 +1,13 @@
+// FIREBASE AUTH 
+import {signInWithGoogle} from '../../../Firebase/Firebase.utils.js';
 // FONTAWESOME ICON LIBRARY COMPONENT
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
 
 function Button({className, icon, text}) {
   return (
     <>
-      <button className={`wrap-btn ${className}`} type="button">
+      <button onClick={() => signInWithGoogle()} className={`wrap-btn ${className}`} type="button">
         <FontAwesomeIcon icon={icon} />
         <span className="wrap-btn__text">{text}</span>
       </button>
