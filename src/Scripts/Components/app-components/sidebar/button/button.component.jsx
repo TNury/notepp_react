722b940f6bcd7a9a7623716ee529fe575bfc4ed2
TNury <yurdesou@gmail.com>
2021-03-02 09:24:01 -0300

    changed CR handling for pass-down-prop handling

--- src/Scripts/Components/app-components/sidebar/button/button.component.jsx
+++ src/Scripts/Components/app-components/sidebar/button/button.component.jsx
@@ -3,22 +3,13 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 // FIREBASE AUTH
 import {auth} from '../../../../Firebase/Firebase.utils.js';
 
-function Button ({text, icon}) {
+function Button ({text, icon, handler}) {
   return (
     <>
-      {
-        text == "Logout"
-        ?
-        <button onClick={() => auth.signOut()} className="sidebar-button">
-          <FontAwesomeIcon className="sidebar-button__icon" icon={icon} />
-          <span className="sidebar-button__text">{text}</span>
-        </button>
-        :
-        <button className="sidebar-button">
-          <FontAwesomeIcon className="sidebar-button__icon" icon={icon} />
-          <span className="sidebar-button__text">{text}</span>
-        </button>
-      }
+      <button onClick={handler} className="sidebar-button">
+        <FontAwesomeIcon className="sidebar-button__icon" icon={icon} />
+        <span className="sidebar-button__text">{text}</span>
+      </button>
     </>
   )
 }
