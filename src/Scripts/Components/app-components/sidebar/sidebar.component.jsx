515381c6151b3633be589ecf0354e55f1b70d0c9
TNury <yurdesou@gmail.com>
2021-03-02 09:24:19 -0300

    added a placeholder handling for dyslexia button

--- src/Scripts/Components/app-components/sidebar/sidebar.component.jsx
+++ src/Scripts/Components/app-components/sidebar/sidebar.component.jsx
@@ -1,12 +1,14 @@
 // COMPONENTS
 import {Button} from './button/button.component.jsx';
+// FIREBASE AUTH
+import {auth} from '../../../Firebase/Firebase.utils.js';
+// HANDLERS
+import {dyslexia} from '../handlers/dyslexia.js';
 // FONTAWESOME LIBRARY DEFAULT ICON
 import {
-  faAngleDoubleRight, faStickyNote, 
-  faSearch, faTint, faAddressCard,
-  faSignOutAlt,faCogs
+  faEye,faStickyNote, faSearch, faTint, 
+  faAddressCard, faSignOutAlt, faCogs
 } from '@fortawesome/free-solid-svg-icons';
-
 
 function Sidebar() {
   return (
@@ -14,8 +16,9 @@ function Sidebar() {
       <Button text="Notes" icon={faStickyNote}/>
       <Button text="Search" icon={faSearch}/>
       <Button text="Themes" icon={faTint}/>
+      <Button handler={() => dyslexia()} text="Dyslexia" icon={faEye}/>
       <Button text="About" icon={faAddressCard}/>
-      <Button text="Logout" icon={faSignOutAlt}/>
+      <Button handler={() => auth.signOut()} text="Logout" icon={faSignOutAlt}/>
       <Button text="Configs" icon={faCogs}/>
     </nav>
   )
