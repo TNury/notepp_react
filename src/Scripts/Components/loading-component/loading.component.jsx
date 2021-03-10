458cf363027f2a7d998237ccca81697bc3ae5a38
TNury <yurdesou@gmail.com>
2021-03-10 07:55:51 -0300

    new loading component and its styles

---
+++ src/Scripts/Components/loading-component/loading.component.jsx
@@ -0,0 +1,14 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCog} from '@fortawesome/free-solid-svg-icons';
+
+
+export const Loading = () => {
+  return (
+    <>
+      <div className="loading">
+        <p className="loading__text">Awaiting Login</p>
+        <FontAwesomeIcon className="loading__spinner" icon={faCog}/>
+      </div>
+    </>
+  )
+}
