f10c4f2df6271d8ae78abef95de351f548af42bf
TNury <yurdesou@gmail.com>
2021-03-17 10:40:14 -0300

    extended app component into class

--- src/Scripts/Components/app-components/app.component.jsx
+++ src/Scripts/Components/app-components/app.component.jsx
@@ -1,19 +1,37 @@
+import React from 'react';
+
+
 // COMPONENTS
 import {Sidebar} from './sidebar/sidebar.component.jsx';
 import {Notes} from './notes/notes.component.jsx';
 import {Note} from './note/note.component.jsx';
 
 
-function App(currentUser) {
-  return (
-    <div className="App">
-      <Sidebar />
-      <div className="Wrap">
-        <Notes currentUser={currentUser}/>
-        <Note />
+class App extends React.Component {
+  constructor({currentUser}) {
+    super();  
+    this.state = {
+      user: currentUser,
+      displayComponent: false
+    }
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Sidebar />
+        <div className="Wrap">
+          <Notes title='Hello, World!' body='Lorem Ipsum' user={this.state.user}/>
+          <Note title='Hello, World!' body='Lorem Ipsum' display={this.state.displayComponent}/>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
+  
 }
 
 export {App};
