666e64f99e02cd2371d2b9bd7cbee3c9753d1251
TNury <yurdesou@gmail.com>
2021-03-17 10:41:02 -0300

    still on the works

--- src/Scripts/Components/app-components/notes/notePrev-component/notePrev.component.jsx
+++ src/Scripts/Components/app-components/notes/notePrev-component/notePrev.component.jsx
@@ -1,11 +1,11 @@
 
 
-export const NotePrev = () => {
+export const NotePrev = ({title, body}) => {
   return(
     <>
       <div className="notes-prev">
-        <p className="notes-prev__title">Title</p>
-        <p className="notes-prev__body">Body</p>
+        <p className="notes-prev__title">{title}</p>
+        <p className="notes-prev__body">{body}</p>
       </div>
       <hr className="notes__line" />
     </>
--- src/Scripts/Components/app-components/notes/notes.component.jsx
+++ src/Scripts/Components/app-components/notes/notes.component.jsx
@@ -1,41 +1,75 @@
 // REACT
 import React from 'react';
 // FONTAWESOME REACT LIBRARY COMPONENT
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // FONTAWESOME LIBRARY DEFAULT ICON
-import {faPlus} from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 // COMPONENTS
-import {NotePrev} from './notePrev-component/notePrev.component.jsx';
+import { NotePrev } from './notePrev-component/notePrev.component.jsx';
 // FIRESTORE
-import {db} from '../../../Firebase/Firebase.utils.js';
+import { db } from '../../../Firebase/Firebase.utils.js';
 
 
 class Notes extends React.Component {
-  constructor({currentUser}) {
+  constructor({user}) {
     super();
     this.state = {
-      showComponent: false
-    }
-    this.user = currentUser;
-    this.handler = this.handler.bind(this);
+      collection: db.collection(user.uid),
+      // notesData: [],
+      // noteNumber: 0,
+      title: '',
+      body: ''
+    };
   }
 
-  handler() {
-    this.setState({showComponent: true});
+  componentDidMount() {
+
+    // const notes = [];
+
+    // this.state.collection.get().then(notesArray => {
+
+    //   notesArray.forEach(note => {
+
+    //     notes.push({title: note.data().title, body: note.data().body});
+    //     this.setState({notesData: notes});
+  
+    //   })
+  
+    // })
   }
 
-  writeUserNotes() {
-    db.collection(`${this.user.currentUser.uid}`).doc('Test').set({
-      test: "Hiiii"
-    });
+  handleNoteChange() {
+
+    const title = document.querySelector('.note__title');
+    const body = document.querySelector('.note__body');
+
+
+    title.addEventListener('keydown', (event) => {
+      this.setState({ title: event.target.value })
+    })
+
+    body.addEventListener('keydown', (event) => {
+      this.setState({ body: event.target.value })
+    })
+
+  }
+
+  componentWillUnmount() {
+    this.setState({ title: '', body: '' })
+  }
+
+  newNote() {
+    
   }
 
   render() {
     return (
       <div className="notes">
-        <NotePrev />
-        <NotePrev />
-        <button className="notes-create">
+        {/* {this.state.notesData.map((note, index) => ( */}
+          <NotePrev title='Lorem Ipsum' body='Lorem Ipsum dolor sit amet'/>
+          <NotePrev title='Lorem Ipsum' body='Lorem Ipsum dolor sit amet'/>
+        {/* ))} */}
+        <button onClick={() => this.newNote()} className="notes-create">
           <FontAwesomeIcon icon={faPlus} />
         </button>
       </div>
