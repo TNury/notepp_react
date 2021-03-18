a4c3f51397060899116ea76bcc4bb1dc6e59eac4
TNury <yurdesou@gmail.com>
2021-03-18 12:01:18 -0300

    integrated Redux to the project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 // LIBRARIES
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+// REDUX STORE
+import { store } from './scripts/Redux/store/store.js';
 
 // COMPONENTS
-import {Notepp} from './scripts/Notepp.script.jsx';
+import Notepp from './scripts/Notepp.script.jsx';
 
 // PERFORMANCE MEASURING
-import {reportWebVitals} from './scripts/Tools/reportWebVitals';
+// import { reportWebVitals } from './scripts/Tools/reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Notepp />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Notepp />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
