62c6fef060e01812db67a0eba1594afb92ba2a78
TNury <yurdesou@gmail.com>
2021-03-18 16:32:49 -0300

    imported & combined new reducer

--- src/Scripts/Redux/rootReducer.js
+++ src/Scripts/Redux/rootReducer.js
@@ -2,10 +2,12 @@
 import { combineReducers } from 'redux';
 // OUR REDUCERS
 import { userReducer } from './reducers/userReducer.js';
-import { noteReducer } from './reducers/noteReducer.js'
+import { notesReducer } from './reducers/notesReducer.js';
+import { noteReducer } from './reducers/noteReducer.js';
 
 
 export default combineReducers ({
   user: userReducer,
-  note: noteReducer
+  notes: notesReducer,
+  note: noteReducer,
 })
