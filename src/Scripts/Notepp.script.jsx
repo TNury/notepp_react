63cc20ea06fc0dff659efd18c2b70f0715f8a78b
TNury <yurdesou@gmail.com>
2021-03-18 12:00:48 -0300

    format, rmvd constructor, lnkd props & state hdlr

--- src/Scripts/Notepp.script.jsx
+++ src/Scripts/Notepp.script.jsx
@@ -1,39 +1,28 @@
 // REACT
 import React from 'react';
 // REACT-ROUTER COMPONENTS
-import {Route, Switch, Redirect} from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 // FIREBASE AUTH
-import {auth} from './Firebase/Firebase.utils.js';
+import { auth } from './Firebase/Firebase.utils.js';
+// REDUX
+import { connect } from 'react-redux';
+import { setCurrentUser } from './Redux/actions/actions.js';
 // COMPONENTS
-import {App} from './Components/app-components/app.component.jsx';
-import {Home} from './Components/home-components/home.component.jsx';
-import {Login} from './Components/login-components/login.component.jsx';
-import {NotFound} from './Components/not-found-component/notFound.component.jsx';
+import { App } from './Components/app-components/app.component.jsx';
+import { Home } from './Components/home-components/home.component.jsx';
+import { Login } from './Components/login-components/login.component.jsx';
+import { NotFound } from './Components/not-found-component/notFound.component.jsx';
 // APP STYLES 
 import './normalizer.scss';
 import './Notepp.styles.scss';
 
 
-class Notepp extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
+export class Notepp extends React.Component {
 
-  // IDEA: PERHAPS USE LOCAL STORAGE TO CONTROL USER STATE
-
-  unsubscribeFromAuth = null;
-  
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
+    auth.onAuthStateChanged((user) => {
+      this.props.setCurrentUser(user);
+    })
   }
 
   render() {
@@ -42,31 +31,31 @@ class Notepp extends React.Component {
         <Switch>
           {/* HOMEPAGE ROUTE */}
           <Route exact path='/'>
-            <Home currentUser={this.state.currentUser} />
+            <Home />
           </Route>
           {/* APP ROUTE
-            
-            If the user is logged in, the route /app becomes
-            available, if he tries to manually change to 
-            /app he will be redirected to login page
-          */}
+          
+          If the user is logged in, the route /app becomes
+          available, if he tries to manually change to 
+          /app he will be redirected to login page
+        */}
           <Route path='/app'>
-            { this.state.currentUser 
-              ? 
-              <App currentUser={this.state.currentUser} />
+            {this.props.currentUser
+              ?
+              <App />
               :
               // Don't forget to <Redirect /> 
               <></>
             }
           </Route>
           {/* LOGIN ROUTE 
-            
-            If the user is logged in, the /login route becomes
-            unavailable, if he tries to manually change to /login
-            he will be redirected to /app.
-          */}
+          
+          If the user is logged in, the /login route becomes
+          unavailable, if he tries to manually change to /login
+          he will be redirected to /app.
+        */}
           <Route path='/login'>
-            { this.state.currentUser
+            {this.props.currentUser
               ?
               <Redirect from='/login' to='/app' />
               :
@@ -74,10 +63,10 @@ class Notepp extends React.Component {
             }
           </Route>
           {/* 404 ROUTE 
-          
-            If there's no route for the current URL, the 404
-            page gets rendered.
-          */}
+        
+          If there's no route for the current URL, the 404
+          page gets rendered.
+        */}
           <Route path='/*' component={NotFound} />
         </Switch>
       </>
@@ -85,4 +74,13 @@ class Notepp extends React.Component {
   }
 }
 
-export {Notepp};
+
+const mapStateToProps = (currentState) => ({
+  currentUser: currentState.user.currentUser
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Notepp);
