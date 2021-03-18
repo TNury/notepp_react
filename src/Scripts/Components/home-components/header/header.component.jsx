93851f92621f98af8b32764589b87a82128ff497
TNury <yurdesou@gmail.com>
2021-03-18 11:50:27 -0300

    minor format, imported redux & linked user state

--- src/Scripts/Components/home-components/header/header.component.jsx
+++ src/Scripts/Components/home-components/header/header.component.jsx
@@ -1,21 +1,23 @@
 // FIREBASE AUTH
-import {auth} from '../../../Firebase/Firebase.utils.js';
+import { auth } from '../../../Firebase/Firebase.utils.js';
+// REDUX CONNECT
+import { connect } from 'react-redux';
 // REACT-ROUTER COMPONENTS
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
-const Header = ({currentUser}) => {
+const Header = ({ currentUser }) => {
   return (
     <header className="header">
       <nav className="header-nav">
         <Link className="header-nav__link header-nav__logo" to="/">Notepp</Link>
-        {
-          currentUser ? // if
-            <>
-              <button onClick={() => auth.signOut()} className="header-nav__button header-nav__item">Logout</button>
-              <Link className="header-nav__link header-nav__item" to="/app">Go To App</Link>
-            </>
-          : // else
+        { currentUser 
+          ?
+          <>
+            <button onClick={() => auth.signOut()} className="header-nav__button header-nav__item">Logout</button>
+            <Link className="header-nav__link header-nav__item" to="/app">Go To App</Link>
+          </>
+          :
           <>
             <Link className="header-nav__link header-nav__item _left" to="/login">Login</Link>
             <Link className="header-nav__link header-nav__item" to="/login">Sign Up</Link>
@@ -26,4 +28,8 @@ const Header = ({currentUser}) => {
   )
 }
 
-export {Header};
+const mapStateToProps = (currentState) => ({
+  currentUser: currentState.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
