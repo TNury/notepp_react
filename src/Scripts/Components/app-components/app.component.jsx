858ef24036c8a8c8e178fe38f10f2ba59ba488b8
TNury <yurdesou@gmail.com>
2021-03-18 11:58:44 -0300

    downgraded to F.C & formatting changes

--- src/Scripts/Components/app-components/app.component.jsx
+++ src/Scripts/Components/app-components/app.component.jsx
@@ -1,37 +1,20 @@
 import React from 'react';
-
-
 // COMPONENTS
-import {Sidebar} from './sidebar/sidebar.component.jsx';
-import {Notes} from './notes/notes.component.jsx';
-import {Note} from './note/note.component.jsx';
+import { Sidebar } from './sidebar/sidebar.component.jsx';
+import Notes from './notes/notes.component.jsx';
+import { Note } from './note/note.component.jsx';
 
 
-class App extends React.Component {
-  constructor({currentUser}) {
-    super();  
-    this.state = {
-      user: currentUser,
-      displayComponent: false
-    }
-  }
-
-  componentDidMount() {
-    
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Sidebar />
-        <div className="Wrap">
-          <Notes title='Hello, World!' body='Lorem Ipsum' user={this.state.user}/>
-          <Note title='Hello, World!' body='Lorem Ipsum' display={this.state.displayComponent}/>
-        </div>
+export const App = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      <div className="Wrap">
+        <Notes />
+        <Note />
       </div>
-    )
-  }
-  
+    </div>
+  )
 }
 
-export {App};
+
