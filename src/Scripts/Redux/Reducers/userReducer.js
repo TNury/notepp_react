2fd918424b848a5a42e8675a409aed11a418a24d
TNury <yurdesou@gmail.com>
2021-03-18 11:46:18 -0300

    new user reducer

--- src/Scripts/Redux/Reducers/userReducer.js
+++ src/Scripts/Redux/Reducers/userReducer.js
@@ -1,6 +1,16 @@
+const INITIAL_STATE = {
+  currentUser: null
+}
 
+/* 
 
-const userReducer = (currentState, action) => {
+  currentState = INITIAL_STATE is a ES6 syntax, it means:
+
+  "If currentState is ever undefined, fall back to this value"
+  
+*/ 
+
+export const userReducer = (currentState = INITIAL_STATE, action) => {
   switch(action.type) {
     case 'SET_CURRENT_USER':
       return {
