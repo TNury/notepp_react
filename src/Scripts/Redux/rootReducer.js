3dca1029caf42e7de2089bc929a0a85f868a9293
TNury <yurdesou@gmail.com>
2021-03-18 15:06:12 -0300

    fixed reducer path & name

--- src/Scripts/Redux/rootReducer.js
+++ src/Scripts/Redux/rootReducer.js
@@ -2,10 +2,10 @@
 import { combineReducers } from 'redux';
 // OUR REDUCERS
 import { userReducer } from './reducers/userReducer.js';
-import { noteContentReducer } from './reducers/noteReducer.js'
+import { noteReducer } from './reducers/noteReducer.js'
 
 
 export default combineReducers ({
   user: userReducer,
-  note: noteContentReducer
+  note: noteReducer
 })
