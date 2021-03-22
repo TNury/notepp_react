39d34fe61ba2a50511213992dea8da6984ffbc6f
TNury <yurdesou@gmail.com>
2021-03-22 16:08:47 -0300

    still working the functionality, added a remove notes icon

--- src/Scripts/Components/app-components/notes/notePrev-component/notePrev.component.jsx
+++ src/Scripts/Components/app-components/notes/notePrev-component/notePrev.component.jsx
@@ -1,13 +1,23 @@
+// REACT
+import React from 'react';
+// FONTAWESOME REACT LIBRARY COMPONENT
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinusCircle } from '@fortawesome/free-solid-svg-icons';
 
 
-export const NotePrev = ({title, body}) => {
+export const NotePrev = ({id, title, body}) => {
+  
   return(
     <>
       <div className="notes-prev">
         <p className="notes-prev__title">{title}</p>
         <p className="notes-prev__body">{body}</p>
+        <button onClick={ () => console.log(id) } className="notes-prev__button">
+          <FontAwesomeIcon className="notes-prev__icon" icon={ faMinusCircle } />
+        </button>
       </div>
       <hr className="notes__line" />
     </>
   )
 }
+
