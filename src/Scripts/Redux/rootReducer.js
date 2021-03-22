37c602d03b77ebbd3db50e606ab488daddc26d89
TNury <yurdesou@gmail.com>
2021-03-22 16:07:00 -0300

    removed deleted reducer's path

--- src/Scripts/Redux/rootReducer.js
+++ src/Scripts/Redux/rootReducer.js
@@ -3,11 +3,9 @@ import { combineReducers } from 'redux';
 // OUR REDUCERS
 import { userReducer } from './reducers/userReducer.js';
 import { notesReducer } from './reducers/notesReducer.js';
-import { noteReducer } from './reducers/noteReducer.js';
 
 
 export default combineReducers ({
   user: userReducer,
-  notes: notesReducer,
-  note: noteReducer,
+  notes: notesReducer
 })
