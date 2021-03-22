831c1d0e92b8e31381c74d385cea0f381a8365fa
TNury <yurdesou@gmail.com>
2021-03-22 16:09:20 -0300

    new comment & minor formatting changes

--- src/Scripts/Components/app-components/notes/notes.component.jsx
+++ src/Scripts/Components/app-components/notes/notes.component.jsx
@@ -2,43 +2,89 @@
 import React from 'react';
 // REDUX 
 import { connect } from 'react-redux';
-import { setNoteTitle, setNoteBody, setNotesCollection } from '../../../Redux/actions/actions.js';
+import { setNoteContent } from '../../../Redux/actions/actions.js';
 // FONTAWESOME REACT LIBRARY COMPONENT
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // FONTAWESOME LIBRARY DEFAULT ICON
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 // COMPONENTS
 import { NotePrev } from './notePrev-component/notePrev.component.jsx';
-// FIRESTORE
+// FIRESTORE DB
 import { db } from '../../../Firebase/Firebase.utils.js';
+// TEST
 
+
+/*
+=================================================================================
+
+      You were working on the delete note button, but since that failed 
+      miserably i suggest you work on that idea where you render textareas
+      based on the note-prev you clicked.
+
+=================================================================================
+
+*/ 
 
 class Notes extends React.Component {
 
   componentDidMount() {
-    const docsRef = db.collection(this.props.user.uid);
 
-    docsRef.get().then((docsArray => {
-      const notes = this.props.notes;
-      this.props.setNotesCollection(docsArray.docs);
-    }))
+    this.loadNotes(this.props.currentUser);
+    
+  }
 
-    docsRef.doc('test').get().then(doc => {
+  newNote(currentUser) {
 
-      this.props.setNoteTitle(doc.data().title);
-      this.props.setNoteBody(doc.data().body);
+    const notesRef = db.collection(`users/${currentUser.id}/notes`);
+    const size = this.props.notesCollection.docs.length;
+
+
+    notesRef.doc().set({
+      id: size, 
+      title: `${size}`, 
+      body: ''
+    })
+    
+    this.loadNotes(currentUser);
+  }
+
+  loadNotes(currentUser) {
+    // NOTES DB REFERENCE
+    const notesRef = db.collection(`users/${currentUser.id}/notes`).orderBy('id');
+
+    // TESTING
+    const notes = [];
+
+    notesRef.get().then(notesCollection => {
+
+      notesCollection.docs.forEach(doc => {
+
+        let note = {
+          id: doc.data().id,
+          title: doc.data().title,
+          body: doc.data().body
+        }
+
+        notes.push(note);
+        this.props.setNoteContent(notes);
+      })
 
     })
-
-    
-
   }
 
   render() {
     return (
       <div className="notes">
-        <NotePrev title={this.props.title} body={this.props.body} />
-        <button onClick={() => this.newNote()} className="notes-create">
+
+      {
+
+        this.props.notesCollection.docs.map(data => (
+          <NotePrev key={data.id} id={data.id} title={data.title} body={data.body}/>
+        ))
+        
+      }
+    
+        <button onClick={() => this.newNote(this.props.currentUser)} className="notes-create">
           <FontAwesomeIcon icon={faPlus} />
         </button>
       </div>
@@ -47,16 +93,12 @@ class Notes extends React.Component {
 }
 
 const mapStateToProps = (currentState) => ({
-  user: currentState.user.currentUser,
-  notes: currentState.notes.notes,
-  title: currentState.note.title,
-  body: currentState.note.body
+  currentUser: currentState.user.currentUser,
+  notesCollection: currentState.notes.notesCollection
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  setNotesCollection: (notes) => dispatch(setNotesCollection(notes)),
-  setNoteTitle: (title) => dispatch(setNoteTitle(title)),
-  setNoteBody: (body) => dispatch(setNoteBody(body))
-})
+  setNoteContent: content => (dispatch(setNoteContent(content)))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Notes);
