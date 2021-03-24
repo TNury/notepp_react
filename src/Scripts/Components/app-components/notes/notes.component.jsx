e6e463ea3f333c5378452718f97613c823aa5c2b
TNury <yurdesou@gmail.com>
2021-03-24 16:39:58 -0300

    integrated Redux to notes component

--- src/Scripts/Components/app-components/notes/notes.component.jsx
+++ src/Scripts/Components/app-components/notes/notes.component.jsx
@@ -2,89 +2,71 @@
 import React from 'react';
 // REDUX 
 import { connect } from 'react-redux';
-import { setNoteContent } from '../../../Redux/actions/actions.js';
+import { setNotesCollection } from '../../../Redux/actions/actions.js';
 // FONTAWESOME REACT LIBRARY COMPONENT
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // FONTAWESOME LIBRARY DEFAULT ICON
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 // COMPONENTS
-import { NotePrev } from './notePrev-component/notePrev.component.jsx';
+import NotePrev from './notePrev-component/notePrev.component.jsx';
 // FIRESTORE DB
 import { db } from '../../../Firebase/Firebase.utils.js';
-// TEST
-
 
 /*
-=================================================================================
-
-      You were working on the delete note button, but since that failed 
-      miserably i suggest you work on that idea where you render textareas
-      based on the note-prev you clicked.
-
-=================================================================================
-
-*/ 
+  For some reason this classe's methods can't access the keyword 'this',
+  so i'm passing props to them through arguments.
+*/
 
 class Notes extends React.Component {
 
   componentDidMount() {
-
-    this.loadNotes(this.props.currentUser);
-    
+    this.loadNotes(this.props);
   }
 
-  newNote(currentUser) {
+  newNote(props) {
 
-    const notesRef = db.collection(`users/${currentUser.id}/notes`);
-    const size = this.props.notesCollection.docs.length;
+    const notesRef = db.collection(`users/${props.currentUser.displayName}/notes`);
 
+    let id = 0;
+
+    props.notesCollection.docs.forEach(() => {
+      id++
+    });
 
     notesRef.doc().set({
-      id: size, 
-      title: `${size}`, 
-      body: ''
-    })
+      id: id, 
+      title: `${id}`, 
+      body: '',
+      isNote: true
+    });
     
-    this.loadNotes(currentUser);
   }
 
-  loadNotes(currentUser) {
-    // NOTES DB REFERENCE
-    const notesRef = db.collection(`users/${currentUser.id}/notes`).orderBy('id');
+  loadNotes(props) {
+    
+    const notesRef = db.collection(`users/${props.currentUser.displayName}/notes`).orderBy('id');
 
-    // TESTING
-    const notes = [];
-
-    notesRef.get().then(notesCollection => {
-
-      notesCollection.docs.forEach(doc => {
-
-        let note = {
-          id: doc.data().id,
-          title: doc.data().title,
-          body: doc.data().body
-        }
-
-        notes.push(note);
-        this.props.setNoteContent(notes);
-      })
-
+    notesRef.where('isNote', '==', true).onSnapshot(({docs}) => {
+      props.setNotesCollection(docs);
     })
+   
   }
 
   render() {
     return (
       <div className="notes">
-
-      {
-
-        this.props.notesCollection.docs.map(data => (
-          <NotePrev key={data.id} id={data.id} title={data.title} body={data.body}/>
-        ))
-        
-      }
-    
-        <button onClick={() => this.newNote(this.props.currentUser)} className="notes-create">
+        {
+          this.props.notesCollection.docs.map((doc, index) => (
+            <NotePrev 
+              key={index}
+              currentUser={this.props.currentUser} 
+              id={doc.data().id} 
+              title={doc.data().title} 
+              body={doc.data().body} 
+            />
+          ))
+        }
+        <button onClick={ () => this.newNote(this.props) } className="notes-create">
           <FontAwesomeIcon icon={faPlus} />
         </button>
       </div>
@@ -92,13 +74,10 @@ class Notes extends React.Component {
   }
 }
 
-const mapStateToProps = (currentState) => ({
+// PROPS
+const mapStatesToProps = (currentState) => ({
   currentUser: currentState.user.currentUser,
   notesCollection: currentState.notes.notesCollection
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  setNoteContent: content => (dispatch(setNoteContent(content)))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Notes);
+export default connect(mapStatesToProps, { setNotesCollection })(Notes);
