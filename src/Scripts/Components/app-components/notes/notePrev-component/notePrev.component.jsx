ee94619b78a546d349c981983261b380b611e4fd
TNury <yurdesou@gmail.com>
2021-03-24 16:38:30 -0300

    integrated redux to component & added note-deleting function

--- src/Scripts/Components/app-components/notes/notePrev-component/notePrev.component.jsx
+++ src/Scripts/Components/app-components/notes/notePrev-component/notePrev.component.jsx
@@ -1,18 +1,39 @@
 // REACT
 import React from 'react';
+// REDUX
+import { connect } from 'react-redux';
+import { setNotesCollection } from '../../../../Redux/actions/actions.js';
 // FONTAWESOME REACT LIBRARY COMPONENT
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+// FIREBASE DB
+import { db } from '../../../../Firebase/Firebase.utils.js';
 
 
-export const NotePrev = ({id, title, body}) => {
+const NotePrev = ({id, currentUser, title, body}) => {
+
+  function deleteNote() {
+
+    const notesRef = db.collection(`users/${currentUser.displayName}/notes`);
+
+    notesRef.where('id', '==', id)
+      .get()
+      .then((query) => {
+
+        query.docs.forEach((doc) => {
+          doc.ref.delete();
+        })
+
+      })
   
+  }
+
   return(
     <>
       <div className="notes-prev">
-        <p className="notes-prev__title">{title}</p>
-        <p className="notes-prev__body">{body}</p>
-        <button onClick={ () => console.log(id) } className="notes-prev__button">
+        <p className="notes-prev__title">{ title }</p>
+        <p className="notes-prev__body">{ body }</p>
+        <button onClick={ deleteNote } className="notes-prev__button">
           <FontAwesomeIcon className="notes-prev__icon" icon={ faMinusCircle } />
         </button>
       </div>
@@ -20,4 +41,10 @@ export const NotePrev = ({id, title, body}) => {
     </>
   )
 }
+
+
+export default connect(null, { setNotesCollection })(NotePrev);
+
+
+
 
