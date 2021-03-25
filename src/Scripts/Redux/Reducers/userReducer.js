ce75024f3ea3d20d5454885673164ae7693e80c7
TNury <yurdesou@gmail.com>
2021-03-24 21:41:39 -0300

    imported & attached action type

--- src/Scripts/Redux/Reducers/userReducer.js
+++ src/Scripts/Redux/Reducers/userReducer.js
@@ -1,18 +1,12 @@
+import { SET_CURRENT_USER } from '../actions/types';
+
 const INITIAL_STATE = {
   currentUser: null
 }
 
-/* 
-
-  currentState = INITIAL_STATE is a ES6 syntax, it means:
-
-  "If currentState is ever undefined, fall back to this value"
-  
-*/ 
-
 export const userReducer = (currentState = INITIAL_STATE, action) => {
   switch(action.type) {
-    case 'SET_CURRENT_USER':
+    case SET_CURRENT_USER:
       return {
         ...currentState,
         currentUser: action.payload
