4281925162620e63a055b33bb390c5e74614e349
TNury <yurdesou@gmail.com>
2021-03-24 23:01:23 -0300

    attached new reducer

--- src/Scripts/Redux/rootReducer.js
+++ src/Scripts/Redux/rootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 // OUR REDUCERS
 import { userReducer } from './reducers/userReducer.js';
 import { notesReducer } from './reducers/notesReducer.js';
+import { fontReducer } from './reducers/fontReducer.js';
 
 
 export default combineReducers ({
   user: userReducer,
-  notes: notesReducer
+  notes: notesReducer,
+  font: fontReducer
 })
