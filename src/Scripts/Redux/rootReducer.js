2fb3125c3181f11b1be9e352fa6d4f59752f9a3f
TNury <yurdesou@gmail.com>
2021-03-30 16:11:23 -0300

    added a comment that shows where each reducer is being utilized

--- src/Scripts/Redux/rootReducer.js
+++ src/Scripts/Redux/rootReducer.js
@@ -2,12 +2,44 @@
 import { combineReducers } from 'redux';
 // OUR REDUCERS
 import { userReducer } from './reducers/userReducer.js';
-import { notesReducer } from './reducers/notesReducer.js';
+import { notesCollection } from './reducers/notesCollectionReducer.js';
+import { noteReducer } from './reducers/noteReducer.js';
 import { fontReducer } from './reducers/fontReducer.js';
+
+
+
+/*
+===================================================
+              COMPONENTS THAT USE
+              
+  - userReducer: 
+
+    1. Notepp.script.jsx
+    2. header.component.jsx
+    3. app.component.jsx
+    4. notes.component.jsx
+    5. notes-prev.component.jsx
+
+  - notesReducer
+
+    1. app.component.jsx
+    2. notes.component.jsx
+    3. note.component.jsx
+
+  - displayedNoteReducer
+
+    1. 
+  
+  - fontReducer:
+    1. sidebar.component.jsx
+
+===================================================
+*/
 
 
 export default combineReducers ({
   user: userReducer,
-  notes: notesReducer,
+  notes: notesCollection,
+  note: noteReducer,
   font: fontReducer
 })
