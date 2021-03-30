1bcdf7dccb8bddfd4faa34475af487cdb85039ba
TNury <yurdesou@gmail.com>
2021-03-30 16:08:58 -0300

    removed action types

--- src/Scripts/Redux/Reducers/fontReducer.js
+++ src/Scripts/Redux/Reducers/fontReducer.js
@@ -1,12 +1,10 @@
-import { SET_APP_FONT } from '../actions/types.js';
-
 const INITIAL_STATE = {
   currentFont: 'Gill Sans'
 }
 
 export const fontReducer = (currentState = INITIAL_STATE, action) => {
   switch(action.type) {
-    case SET_APP_FONT: 
+    case 'SET_APP_FONT': 
       return {
         ...currentState,
         currentFont: action.payload
