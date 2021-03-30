64979f9a0dd999018470a656a607dd7adff561f4
TNury <yurdesou@gmail.com>
2021-03-30 16:10:06 -0300

    removed types import and added more actions

--- src/Scripts/Redux/actions/actions.js
+++ src/Scripts/Redux/actions/actions.js
@@ -1,21 +1,55 @@
-import { 
-  SET_NOTES_COLLECTION, 
-  SET_CURRENT_USER,
-  SET_APP_FONT  
-} from './types.js';
-
-
-export const setCurrentUser = (user) => ({
-  type: SET_CURRENT_USER,
+// USER ACTIONS
+const setCurrentUser = (user) => ({
+  type: 'SET_CURRENT_USER',
   payload: user
 });
 
-export const setNotesCollection = (notes) => ({
-  type: SET_NOTES_COLLECTION,
+// NOTES COLLECTION ACTIONS
+const setNotesCollection = (notes) => ({
+  type: 'SET_NOTES_COLLECTION',
   payload: notes
 });
 
-export const setAppFont = (font) => ({
-  type: SET_APP_FONT,
-  payload: font
+const setNotesCollectionRef = (ref) => ({
+  type: 'SET_NOTES_REFERENCE',
+  payload: ref
 })
+
+// APP ACTIONS
+const setAppFont = (font) => ({
+  type: 'SET_APP_FONT',
+  payload: font
+});
+
+
+// NOTE DISPLAY ACTIONS
+const displayNote = (boolean) => ({
+  type: 'DISPLAY_NOTE',
+  payload: boolean
+});
+
+const setNoteId = (id) => ({
+  type: 'SET_NOTE_ID',
+  payload: id
+})
+
+const setNoteTitle = (title) => ({
+  type: 'SET_NOTE_TITLE',
+  payload: title
+});
+
+const setNoteBody = (body) => ({
+  type: 'SET_NOTE_BODY',
+  payload: body
+});
+
+export {
+  setCurrentUser,
+  setNotesCollection,
+  setNotesCollectionRef,
+  displayNote,
+  setNoteId,
+  setNoteTitle,
+  setNoteBody,
+  setAppFont
+};
