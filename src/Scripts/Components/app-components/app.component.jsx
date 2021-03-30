796fbeee035e5ba372dbea84a54806f6cf3aedad
TNury <yurdesou@gmail.com>
2021-03-30 16:14:22 -0300

    extended app to class and imported redux, now app is where the notes collection is defined

--- src/Scripts/Components/app-components/app.component.jsx
+++ src/Scripts/Components/app-components/app.component.jsx
@@ -1,20 +1,67 @@
+// REACT
 import React from 'react';
+// FIREBASE
+import { db } from '../../Firebase/Firebase.utils.js';
+// REDUX
+import { connect } from 'react-redux';
+import { setNotesCollection, setNotesCollectionRef } from '../../Redux/actions/actions.js';
 // COMPONENTS
 import Sidebar from './sidebar/sidebar.component.jsx';
 import Notes from './notes/notes.component.jsx';
-import { Note } from './note/note.component.jsx';
+import Note from './note/note.component.jsx';
 
 
-export const App = () => {
-  return (
-    <div className="App">
-      <Sidebar />
-      <div className="Wrap">
-        <Notes />
-        <Note />
+class App extends React.Component {
+
+  componentDidMount() {
+
+    const { currentUser } = this.props.reduxProps;
+    const { setNotesCollection, setNotesCollectionRef } = this.props.reduxActions;
+
+    const notesRef = db.collection(`users/${currentUser.displayName}/notes`);
+
+    setNotesCollectionRef(notesRef);
+
+    notesRef.orderBy('id').onSnapshot(({docs}) => {
+
+      const notes = [];
+
+      docs.forEach((doc) => {
+        notes.push(doc.data());
+      })
+
+      setNotesCollection(notes);
+
+    });
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Sidebar />
+        <div className="Wrap">
+          <Notes />
+          <Note />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
+// PROPS
+const mapStateToProps = (currentState) => ({
+  reduxProps: {
+    currentUser: currentState.user.value
+  }
+})
 
+const mapDispatchToProps = (dispatch) => ({
+  reduxActions: {
+    setNotesCollection: notes => dispatch(setNotesCollection(notes)),
+    setNotesCollectionRef: ref => dispatch(setNotesCollectionRef(ref))
+  }
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
