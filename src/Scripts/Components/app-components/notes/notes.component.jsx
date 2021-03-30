5209b78c774bbbb256512046c7a95a0cd5fe4180
TNury <yurdesou@gmail.com>
2021-03-30 16:13:27 -0300

    extended notes component to class

--- src/Scripts/Components/app-components/notes/notes.component.jsx
+++ src/Scripts/Components/app-components/notes/notes.component.jsx
@@ -9,90 +9,68 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 // COMPONENTS
 import NotesPrev from './notesPrev-component/notesPrev.component.jsx';
-// FIRESTORE DB
-import { db } from '../../../Firebase/Firebase.utils.js';
 
-/*
-  For some reason this classe's methods can't access the keyword 'this',
-  so i'm passing props to them through arguments.
-*/
+class Notes extends React.Component {
 
-const Notes = (props) => {
+  newNote() {
 
-  const { currentUser, userNotes} = props.reduxProps;
-  const { displayNote } = props.reduxActions; 
-
-  // KEEP AN EYE ON THIS 
-  // (function test() {
-  //   if (Object.keys(userNotes).length === 0) {
-  //     return
-  //   } else {
-  //     userNotes.get().then(data => console.log(data))
-  //   }
-  // })()
-
-  if (Object.keys(userNotes).length === 0) {
-    return
-  } else {
-    userNotes.get().then(data => console.log(data))
-  }
-
-  function newNote() {
-
-    const notesRef = db.collection(`users/${currentUser.displayName}/notes`);
+    const { notesCollection } = this.props.reduxProps;
 
     let id = 0;
 
-    userNotes.docs.forEach(() => {
+    notesCollection.docs.forEach(() => {
       id++
-    });
+    })
 
-    notesRef.doc().set({
-      id: id, 
-      title: `${id}`, 
-      body: '',
-      isNote: true
-    });
+    notesCollection.ref.doc().set({
+      id: id,
+      title: 'New note',
+      body: ''
+    })
+
+  }
+
+  closeNote() {
+    this.props.reduxActions.displayNote(false);
+  }
+
+  render() {
+    const notes = this.props.reduxProps.notesCollection.docs;
     
+    return (
+      <div onClick={() => this.closeNote()} className="notes">
+        {
+          notes.map(({ id, title, body }, index) => (
+            <NotesPrev 
+              key={index}
+              drilledProps={{
+                id: id,
+                title: title,
+                body: body
+              }} 
+            />
+          ))
+        }
+        <button onClick={() => this.newNote()} className="notes-create">
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </div>
+    )
   }
-
-  function closeNote() {
-    displayNote(false);
-  }
-
-
-  return (
-    <div onClick={() => closeNote()} className="notes">
-      {/* {
-        userNotes.docs.map((doc, index) => (
-          <NotesPrev 
-            key={index}
-            drilledProps={{
-              id: doc.data().id,
-              title: doc.data().title,
-              body: doc.data().body
-            }}
-          />
-        ))
-      } */}
-      <button onClick={() => newNote()} className="notes-create">
-        <FontAwesomeIcon icon={faPlus} />
-      </button>
-    </div>
-  )
+  
 }
 
 // PROPS
 const mapStatesToProps = (currentState) => ({
   reduxProps: {
     currentUser: currentState.user.value,
-    userNotes: currentState.notes.notesCollection
+    notesCollection: currentState.notes.notesCollection
   }
 });
 
 const mapDispatchToProps = (dispatch) => ({
   reduxActions: {
-    displayNote: note => dispatch(displayNote(note))
+    displayNote: boolean => dispatch(displayNote(boolean))
   }
 });
 
