0d244de9f1dfd482fdbb0265f3e5f52c0a28c49b
TNury <yurdesou@gmail.com>
2021-03-31 09:13:24 -0300

    changed way of handling clicks from conditional rendering to prop drilled handlers

--- src/Scripts/Components/login-components/button-component/button.component.jsx
+++ src/Scripts/Components/login-components/button-component/button.component.jsx
@@ -1,25 +1,14 @@
-// FIREBASE AUTH 
-import { signInWithGoogle } from '../../../Firebase/Firebase.utils.js';
 // FONTAWESOME ICON LIBRARY COMPONENT
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
-export const Button = ({ className, icon, text }) => {
+export const Button = ({ handler, className, icon, text }) => {
   return (
     <>
-      { 
-        text === 'Google' 
-        ?
-        <button onClick={() => signInWithGoogle()} className={`wrap-btn ${ className }`} type="button">
-          <FontAwesomeIcon icon={ icon } />
-          <span className="wrap-btn__text">{ text }</span>
-        </button>
-        :
-        <button className={`wrap-btn ${ className }`} type="button">
-          <FontAwesomeIcon icon={ icon } />
-          <span className="wrap-btn__text">{ text }</span>
-        </button>
-      }
+      <button onClick={ handler } className={`wrap-btn ${ className }`} type="button">
+        <FontAwesomeIcon icon={ icon } />
+        <span className="wrap-btn__text">{ text }</span>
+      </button>
     </>
   )
 }
