c81ece88680af301db363dad9e2a95de4c057636
TNury <yurdesou@gmail.com>
2021-04-02 14:26:31 -0300

    removed unused import, imported fontawesome component & icons, changed 'text' prop name to 'location', changed conditional render paremeter.

--- src/Scripts/Components/reusable-components/header/header.component.jsx
+++ src/Scripts/Components/reusable-components/header/header.component.jsx
@@ -1,40 +1,41 @@
-// FIREBASE AUTH
-import { auth } from '../../../Firebase/Firebase.utils.js';
 // REDUX CONNECT
 import { connect } from 'react-redux';
 // REACT-ROUTER COMPONENTS
 import { Link } from 'react-router-dom';
-
+// FONTAWESOME COMPONENT
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDoorOpen, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Header = (props) => {
 
   const isLoggedIn = props.reduxProps.currentUserProp;
-  const text = props.drilledProps.text;
+  const location = props.drilledProps.location;
 
   return (
-    <>
-      <header className="header">
-        <nav className="header-nav">
-          <Link className="header-nav__link header-nav__logo" to="/">Notepp</Link>
-          { 
-            isLoggedIn 
-            ?
-            <>
-              <button onClick={() => auth.signOut()} className="header-nav__button header-nav__item">Logout</button>
-              <Link className="header-nav__link header-nav__item" to="/app">Go To App</Link>
-            </>
-            :
-            <Link className="header-nav__link header-nav__item _left" to="/login">{ text }</Link>       
+    <header className="header">
+      {
+        location === 'login'
+          ?
+        <nav className="navbar _centralize">
+          <Link className="navbar__link navbar__logo" to="/">Notepp</Link>
+        </nav>
+          :
+        <nav className="navbar">
+          <Link className="navbar__link navbar__logo" to="/">Notepp</Link>
+          {
+            isLoggedIn
+              ?
+            <Link className="navbar__link navbar-btn" to="/app">
+              <FontAwesomeIcon className="navbar-btn__icon" icon={faDoorOpen} />
+            </Link>
+              :
+            <Link className="navbar__link navbar-btn" to="/login">
+              <FontAwesomeIcon className="navbar-btn__icon" icon={faSignInAlt} />
+            </Link>
           }
         </nav>
-      </header>
-      <div className="alert">
-        <span className="alert__text">
-          Hello there! As i'm still working on the app there might be some 
-          bugs so be warned!
-        </span>
-      </div>
-    </>
+      }
+    </header>
   )
 }
 
