037d7a0cca115dcc6180a33456777950e45e157a
TNury <yurdesou@gmail.com>
2021-04-06 17:21:50 -0300

    added header & footer

--- src/Scripts/Components/home-components/home.component.jsx
+++ src/Scripts/Components/home-components/home.component.jsx
@@ -3,16 +3,23 @@ import { faStickyNote } from '@fortawesome/free-solid-svg-icons';
 
 // COMPONENTS 
 import { Main } from './main/main.component.jsx'
+import Header from '../reusable-components/header/header.component.jsx';
+import { Footer } from '../reusable-components/footer/footer.component.jsx';
+
 
 export const Home = () => {
 
   return (
     <div className="home">
+      <Header drilledProps={{
+        location: 'home'
+      }} />
       <div className="banner">
         <FontAwesomeIcon className="banner__icon" icon={faStickyNote} />
         <h1 className="banner__head">Welcome to Notepp!</h1>
       </div>
       <Main />
+      <Footer />
     </div>
   )
 }
