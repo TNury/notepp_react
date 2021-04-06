d7e1aff06068a6345daf6314363e5203a694d15d
TNury <yurdesou@gmail.com>
2021-04-06 08:26:06 -0300

    removed unused empty tag

--- src/Scripts/Components/login-components/login.component.jsx
+++ src/Scripts/Components/login-components/login.component.jsx
@@ -20,29 +20,27 @@ import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 export const Login = () => {
   return(
-    <>
-      <div className="login-wrap">
-        <form className="login">
-          <h2 className="login__head">Login with</h2>
-          <div className="btn-group">
-            <Button handler={() => signInWithGoogle()} className="_brand-g" icon={ faGoogle } text="Google" />
-            <Button handler={() => signInWithTwitter()} className="_brand-t" icon={ faTwitter } text="Twitter" />
-            <Button handler={() => signInWithFacebook()} className="_brand-f" icon={ faFacebookF } text="Facebook" />
-            <Button handler={() => signInWithGithub()} className="_brand-a" icon={ faGithub } text="GitHub" />
-          </div>
-          <div className="divider">
-            <span className="divider__text">Or</span>
-            <span className="divider__line"></span>
-          </div>
-          <div className="input-group">
-            <label htmlFor="email" className="input-group__item _label">Email</label>
-            <input id="email" className="input-group__item _input" type="email" placeholder="jon-doe@something.com"></input>
-            <label htmlFor="password" className="input-group__item _label">Password</label>
-            <input id="password" className="input-group__item _input" type="password" placeholder="12345"></input>
-          </div>
-          <Button className="login__submit-btn" icon={ faSignInAlt } text="Submit" />
-        </form>
-      </div>
-    </>
+    <div className="login-wrap">
+      <form className="login">
+        <h2 className="login__head">Login with</h2>
+        <div className="btn-group">
+          <Button handler={() => signInWithGoogle()} className="_brand-g" icon={ faGoogle } text="Google" />
+          <Button handler={() => signInWithTwitter()} className="_brand-t" icon={ faTwitter } text="Twitter" />
+          <Button handler={() => signInWithFacebook()} className="_brand-f" icon={ faFacebookF } text="Facebook" />
+          <Button handler={() => signInWithGithub()} className="_brand-a" icon={ faGithub } text="GitHub" />
+        </div>
+        <div className="divider">
+          <span className="divider__text">Or</span>
+          <span className="divider__line"></span>
+        </div>
+        <div className="input-group">
+          <label htmlFor="email" className="input-group__item _label">Email</label>
+          <input id="email" className="input-group__item _input" type="email" placeholder="jon-doe@something.com"></input>
+          <label htmlFor="password" className="input-group__item _label">Password</label>
+          <input id="password" className="input-group__item _input" type="password" placeholder="12345"></input>
+        </div>
+        <Button className="login__submit-btn" icon={ faSignInAlt } text="Submit" />
+      </form>
+    </div>
   )
 }
