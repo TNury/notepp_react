61869211899d6f0ad86060caa19480b70a8b8c32
TNury <yurdesou@gmail.com>
2021-04-07 09:27:56 -0300

    Changed classname & shortened span text

--- src/Scripts/Components/not-found-component/notFound.component.jsx
+++ src/Scripts/Components/not-found-component/notFound.component.jsx
@@ -6,13 +6,13 @@ import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 export const NotFound = () => {
   return(
-    <div className="not-found">
-      <div className="not-found-div">
+    <div className="not-found-container">
+      <div className="not-found">
         <h1 className="not-found__head">
           Error 404 <FontAwesomeIcon icon={ faExclamationTriangle } />
         </h1>
         <p className="not-found__text">
-          This page doesn't exist or you need to be logged in to see it.
+          This page doesn't exist.
         </p>
       </div>
     </div>
