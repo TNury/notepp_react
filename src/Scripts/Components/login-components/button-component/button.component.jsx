fd219dae78ad1e3176424da729b77ce867407516
TNury <yurdesou@gmail.com>
2021-04-07 13:30:28 -0300

    updated buttons to receive prop type

--- src/Scripts/Components/login-components/button-component/button.component.jsx
+++ src/Scripts/Components/login-components/button-component/button.component.jsx
@@ -1,15 +1,13 @@
 // FONTAWESOME ICON LIBRARY COMPONENT
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-
-export const Button = ({ handler, className, icon, text }) => {
+export const Button = ({ type, handler, className, icon, text }) => {
+  
   return (
-    <>
-      <button onClick={ handler } className={`btn-group-btn ${ className }`} type="button">
-        <FontAwesomeIcon className="btn-group-btn__icon" icon={ icon } />
-        <span className="btn-group-btn__text">{ text }</span>
-      </button>
-    </>
+    <button type={type} onClick={ handler } className={`btn-group-btn ${ className }`} >
+      <FontAwesomeIcon className="btn-group-btn__icon" icon={ icon } />
+      <span className="btn-group-btn__text">{ text }</span>
+    </button>
   )
 }
 
