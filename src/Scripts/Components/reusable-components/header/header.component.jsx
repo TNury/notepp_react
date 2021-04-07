68e950059d73ce5b96a9d321cdcd0a0a0b5bd753
TNury <yurdesou@gmail.com>
2021-04-07 10:54:44 -0300

    removed location prop & unused code

--- src/Scripts/Components/reusable-components/header/header.component.jsx
+++ src/Scripts/Components/reusable-components/header/header.component.jsx
@@ -9,32 +9,12 @@ import { faDoorOpen, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 const Header = (props) => {
 
   const isLoggedIn = props.reduxProps.currentUserProp;
-  const location = props.drilledProps.location;
 
   return (
     <header className="header">
-      {
-        location === 'login'
-          ?
-        <nav className="navbar _centralize">
-          <Link className="navbar__link navbar__logo" to="/">Notepp</Link>
-        </nav>
-          :
-        <nav className="navbar">
-          <Link className="navbar__link navbar__logo _centralize" to="/">Notepp</Link>
-          {/* {
-            isLoggedIn
-              ?
-            <Link className="navbar__link navbar-btn" to="/app">
-              <FontAwesomeIcon className="navbar-btn__icon" icon={faDoorOpen} />
-            </Link>
-              :
-            <Link className="navbar__link navbar-btn" to="/login">
-              <FontAwesomeIcon className="navbar-btn__icon" icon={faSignInAlt} />
-            </Link>
-          } */}
-        </nav>
-      }
+      <nav className="navbar">
+        <Link className="navbar__link navbar__logo" to="/">Notepp</Link>
+      </nav>
     </header>
   )
 }
