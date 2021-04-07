cee187affba0e760e76a05ded9eaa83ff4787319
TNury <yurdesou@gmail.com>
2021-04-07 16:06:38 -0300

    renamed file

--- src/Scripts/Components/login-components/button-group/button/button.jsx
+++ src/Scripts/Components/login-components/button-group/button/button.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export const Button = ({ type, handler, className, icon, text }) => {
   
   return (
-    <button type={type} onClick={ handler } className={`btn-group-btn ${ className }`} >
+    <button type={ type } onClick={ handler } className={`btn-group-btn ${ className }`} >
       <FontAwesomeIcon className="btn-group-btn__icon" icon={ icon } />
       <span className="btn-group-btn__text">{ text }</span>
     </button>
