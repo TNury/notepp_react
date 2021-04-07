dc1148d66afef1ef5b815a98552b4c99e27b7584
TNury <yurdesou@gmail.com>
2021-04-07 13:30:13 -0300

    added a new prop to buttons (type)

--- src/Scripts/Components/login-components/login.component.jsx
+++ src/Scripts/Components/login-components/login.component.jsx
@@ -7,7 +7,7 @@ import {
 } from '../../Firebase/Firebase.utils.js';
 // COMPONENTS
 import { Button } from './button-component/button.component.jsx';
-import Header from '../reusable-components/header/header.component.jsx';
+import { Header } from '../reusable-components/header/header.component.jsx';
 import { Footer } from '../reusable-components/footer/footer.component.jsx';
 // FONTAWESOME BRAND LIBRARY ICON
 import { 
@@ -21,16 +21,17 @@ import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 
 export const Login = () => {
+  
   return(
     <div className="login-wrap">
       <Header />
       <form className="login">
         <h2 className="login__head">Login with</h2>
         <div className="btn-group">
-          <Button handler={() => signInWithGoogle()} className="_brand-g" icon={ faGoogle } text="Google" />
-          <Button handler={() => signInWithTwitter()} className="_brand-t" icon={ faTwitter } text="Twitter" />
-          <Button handler={() => signInWithFacebook()} className="_brand-f" icon={ faFacebookF } text="Facebook" />
-          <Button handler={() => signInWithGithub()} className="_brand-a" icon={ faGithub } text="GitHub" />
+          <Button type="button" handler={() => signInWithGoogle()} className="_brand-g" icon={ faGoogle } text="Google" />
+          <Button type="button" handler={() => signInWithTwitter()} className="_brand-t" icon={ faTwitter } text="Twitter" />
+          <Button type="button" handler={() => signInWithFacebook()} className="_brand-f" icon={ faFacebookF } text="Facebook" />
+          <Button type="button" handler={() => signInWithGithub()} className="_brand-a" icon={ faGithub } text="GitHub" />
         </div>
         <div className="divider">
           <span className="divider__text">Or</span>
@@ -38,11 +39,11 @@ export const Login = () => {
         </div>
         <div className="input-group">
           <label htmlFor="email" className="input-group__item _label">Email</label>
-          <input id="email" className="input-group__item _input" type="email" placeholder="jon-doe@something.com"></input>
+          <input id="email" className="input-group__item _input" type="email" placeholder="jon-doe@something.com" required />
           <label htmlFor="password" className="input-group__item _label">Password</label>
-          <input id="password" className="input-group__item _input" type="password" placeholder="12345"></input>
+          <input id="password" className="input-group__item _input" type="password" placeholder="12345" required />
         </div>
-        <Button className="login__submit-btn" icon={ faSignInAlt } text="Submit" />
+        <Button type="submit" handler={() => console.log('')} className="login__submit-btn" icon={ faSignInAlt } text="Submit" />
       </form>
       <Footer />
     </div>
