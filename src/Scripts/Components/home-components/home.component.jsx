2a68f0efb8fe4b2fd5f1af212daa5bbe8346fee2
TNury <yurdesou@gmail.com>
2021-04-07 08:09:50 -0300

    gave this reduxProps & fontAwesome components

--- src/Scripts/Components/home-components/home.component.jsx
+++ src/Scripts/Components/home-components/home.component.jsx
@@ -1,13 +1,17 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStickyNote } from '@fortawesome/free-solid-svg-icons';
-
+// REDUX
+import { connect } from 'react-redux';
 // COMPONENTS 
 import { Main } from './main/main.component.jsx'
 import Header from '../reusable-components/header/header.component.jsx';
 import { Footer } from '../reusable-components/footer/footer.component.jsx';
+// FONTAWESOME COMPONENTS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStickyNote } from '@fortawesome/free-solid-svg-icons';
 
 
-export const Home = () => {
+const Home = (props) => {
+
+  const isLoggedIn = props.reduxProps.currentUserProp;
 
   return (
     <div className="home">
@@ -17,9 +21,29 @@ export const Home = () => {
       <div className="banner">
         <FontAwesomeIcon className="banner__icon" icon={faStickyNote} />
         <h1 className="banner__head">Welcome to Notepp!</h1>
+        {
+          false
+          ?
+          <div className="btn-group">
+            <button className="btn-group__btn">Go to App!</button>
+          </div>
+          :
+          <div className="btn-group">
+            <button className="btn-group__btn">Login</button>
+            <button className="btn-group__btn">Sign Up</button>
+          </div>
+        }
       </div>
       <Main />
       <Footer />
     </div>
   )
 }
+
+const mapStoreToProps = (currentStore) => ({
+  reduxProps: {
+    currentUserProp: currentStore.userReducer.user
+  }
+})
+
+export default connect(mapStoreToProps)(Home);
