f0806509d7db6a18d1c9e6d2cfff5398d8030d00
TNury <yurdesou@gmail.com>
2021-04-09 09:47:51 -0300

    moved button-group back, minor change to class name

--- src/Scripts/Components/login-components/login.jsx
+++ src/Scripts/Components/login-components/login.jsx
@@ -1,11 +1,14 @@
+// FIREBASE AUTH
+import { signInWithGoogle, signInWithTwitter, signInWithFacebook, signInWithGithub } from '../../Firebase/Firebase.utils.js';
 // COMPONENTS
 import { Header } from '../reusable-components/header/header.jsx';
-import { ButtonGroup } from './button-group/button-group.jsx';
+import { Button } from '../reusable-components/button/button.jsx';
 import { Footer } from '../reusable-components/footer/footer.jsx';
 // FONTAWESOME COMPONENT 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// FONTAWESOME LIBRARY DEFAULT ICON
+// FONTAWESOME ICONS
 import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { faGoogle, faTwitter, faFacebookF, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 
 export const Login = () => {
@@ -15,7 +18,12 @@ export const Login = () => {
       <Header />
       <form className="login">
         <h2 className="login__head">Login with</h2>
-        <ButtonGroup />
+        <div className="button-wrap">
+          <Button type="button" handler={() => signInWithGoogle} modifier="_brand-g" icon={faGoogle} text="Google"/>
+          <Button type="button" handler={() => signInWithTwitter} modifier="_brand-t" icon={faTwitter} text="Twitter"/>
+          <Button type="button" handler={() => signInWithFacebook} modifier="_brand-f" icon={faFacebookF} text="Facebook"/>
+          <Button type="button" handler={() => signInWithGithub} modifier="_brand-git" icon={faGithub} text="GitHub"/>
+        </div>
         <div className="divider">
           <span className="divider__text">Or</span>
           <span className="divider__line"></span>
@@ -26,10 +34,9 @@ export const Login = () => {
           <label htmlFor="password" className="input-group__item _label">Password</label>
           <input id="password" className="input-group__item _input" type="password" placeholder="12345" required />
         </div>
-        <button type="submit" className="submit-btn" >
-          <FontAwesomeIcon className="submit-btn__icon" icon={faSignInAlt} />
-          <span className="submit-btn__text">Submit</span>
-        </button>
+        <div className="button-wrap">
+          <Button type="submit" handler={() => console.log('Hi')} modifier="_submit" icon={faSignInAlt} text="Submit" />
+        </div>
       </form>
       <Footer />
     </div>
