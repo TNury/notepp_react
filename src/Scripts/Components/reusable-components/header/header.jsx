bd51ba008002ceb3b77270d71fd81da753438513
TNury <yurdesou@gmail.com>
2021-04-09 12:44:43 -0300

    new icon, div & span added to navbar, also changed class name

--- src/Scripts/Components/reusable-components/header/header.jsx
+++ src/Scripts/Components/reusable-components/header/header.jsx
@@ -1,12 +1,19 @@
 // REACT-ROUTER COMPONENTS
 import { Link } from 'react-router-dom';
+// FONTAWESOME COMPONENTS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 export const Header = () => {
 
   return (
     <header className="header">
       <nav className="navbar">
-        <Link className="navbar__link navbar__logo" to="/">Notepp</Link>
+        <div className="logo">
+          <Link className="logo__text" to="/">Notepp</Link>
+          <span className="logo__line"></span>
+          <FontAwesomeIcon className="logo__pencil" icon={ faPencilAlt } />
+        </div>
       </nav>
     </header>
   )
