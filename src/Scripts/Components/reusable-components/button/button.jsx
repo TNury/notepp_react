1ee6a09c38949e51813688327fa52506ded62b89
TNury <yurdesou@gmail.com>
2021-04-19 09:17:09 -0300

    added comment, changed attribute position

--- src/Scripts/Components/reusable-components/button/button.jsx
+++ src/Scripts/Components/reusable-components/button/button.jsx
@@ -2,6 +2,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Button = ({ type, handler, modifier, icon, text }) => {
+
+  /*
+  ======================================
+        THIS COMPONENT IS USED IN  
+        
+    1. login.jsx
+    2. sidebar.jsx  
+    3. notesEditor.jsx
+    4. notesCollection.jsx
+    5. note.jsx
+
+  ======================================
+  */ 
   
   /*
     The first ternary operator checks to see if there's a modifier prop,
@@ -15,12 +28,12 @@ export const Button = ({ type, handler, modifier, icon, text }) => {
   return (
     modifier
     ?
-    <button type={ type } onClick={ handler } className={`button ${ modifier }`} >
+    <button  onClick={ handler } className={`button ${ modifier }`} type={ type }>
       <FontAwesomeIcon className="button__icon" icon={ icon } />
       <span className="button__text">{ text }</span>
     </button>
     :
-    <button type={ type } onClick={ handler } className="button" >
+    <button  onClick={ handler } className="button" type={ type }>
       <FontAwesomeIcon className="button__icon" icon={ icon } />
       {
         text
