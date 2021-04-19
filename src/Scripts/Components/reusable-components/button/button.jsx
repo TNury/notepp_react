fd5c3b2f1f27b5c2a85115d6b5316401d2a94e2f
TNury <yurdesou@gmail.com>
2021-04-19 08:58:04 -0300

    gave button conditinal rendering parameters

--- src/Scripts/Components/reusable-components/button/button.jsx
+++ src/Scripts/Components/reusable-components/button/button.jsx
@@ -3,10 +3,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Button = ({ type, handler, modifier, icon, text }) => {
   
+  /*
+    The first ternary operator checks to see if there's a modifier prop,
+    if yes, return the button with the modifier included on className, else, 
+    return a button without it.
+
+    The second one checks to see if there's a text prop, if yes, return a
+    span with the prop as its content, else, return an empty tag.
+  */ 
+  
   return (
+    modifier
+    ?
     <button type={ type } onClick={ handler } className={`button ${ modifier }`} >
       <FontAwesomeIcon className="button__icon" icon={ icon } />
       <span className="button__text">{ text }</span>
+    </button>
+    :
+    <button type={ type } onClick={ handler } className="button" >
+      <FontAwesomeIcon className="button__icon" icon={ icon } />
+      {
+        text
+        ?
+        <span className="button__text">{ text }</span>
+        :
+        <></>
+      }
     </button>
   )
 }
